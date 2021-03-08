3dc88ea0c0214740de4b54cb4f49618c25799969
posiek07 <daniel.smolinski@hotmail.com>
2021-03-08 21:01:05 +0000

    css-deploy-fixes-test2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './slices/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './templates/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false,
   theme: {
